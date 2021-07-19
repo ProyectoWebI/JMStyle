835bd75574457bb39a8b068811ff324da05dbe4a
Jennifer Varela <50208658+JenniferVarela@users.noreply.github.com>
2021-07-18 23:40:06 -0600

    Se agrego pagina de registro y validacion en JS

--- JMStyle_Web/js/funciones.js
+++ JMStyle_Web/js/funciones.js
@@ -34,6 +34,60 @@ function formContato(){
     }else{
         alert("Consulta enviada");
 
+        defectoColor(document.getElementById("nombre"));
+        defectoColor(document.getElementById("email"));
+        defectoColor(document.getElementById("telefono"));
+        defectoColor(document.getElementById("consulta"));
+
+    }
+
+}
+
+//Validacion formulario registro
+function formRegistro(){
+    var nombre,correo,telefono,apellido,direccion, msj;
+
+    nombre = document.getElementById("nombre").value;
+    apellido =  document.getElementById("email").value;
+    telefono = document.getElementById("telefono").value;
+    apellido = document.getElementById("apellido").value;
+    direccion =  document.getElementById("direccion").value;
+
+    defectoColor(document.getElementById("nombre"));
+    defectoColor(document.getElementById("email"));
+    defectoColor(document.getElementById("telefono"));
+    defectoColor(document.getElementById("direccion"));
+    defectoColor(document.getElementById("apellido"));
+
+
+
+    if(nombre == ""){
+
+        msj="Ingrese su nombre";
+        document.getElementById("alerta").innerHTML=msj;
+        alertaColor(document.getElementById("nombre"));
+
+    }else if(apellido ==""){
+        msj="Ingrese su apellido";
+        document.getElementById("alerta").innerHTML=msj;
+        alertaColor(document.getElementById("apellido"));
+        
+    }else if(telefono ==""){
+        msj="Ingrese numero de telefono";
+        document.getElementById("alerta").innerHTML=msj;
+        alertaColor(document.getElementById("telefono"));
+    }else if(direccion ==""){
+        msj="Ingrese su consulta";
+        document.getElementById("alerta").innerHTML=msj;
+        alertaColor(document.getElementById("direccion"));
+    }else{
+        alert("Registrado exitosamente");
+        defectoColor(document.getElementById("nombre"));
+        defectoColor(document.getElementById("email"));
+        defectoColor(document.getElementById("telefono"));
+        defectoColor(document.getElementById("direccion"));
+        defectoColor(document.getElementById("apellido"));
+
     }
 
 }
